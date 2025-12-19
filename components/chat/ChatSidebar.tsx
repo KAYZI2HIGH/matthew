b5fef3abe7e44c31c342ecbc57ca3826f5398819
Kayode <dadavictory2000@gmail.com>
2025-12-19 04:31:57 +0100

    chore: comment out Audits button on sidebar

--- components/chat/ChatSidebar.tsx
+++ components/chat/ChatSidebar.tsx
@@ -77,14 +77,14 @@ export function ChatSidebar({
                   <span>Search Chats</span>
                 </SidebarMenuButton>
               </SidebarMenuItem>
-              <SidebarMenuItem>
+              {/* <SidebarMenuItem>
                 <AuditDialog>
                   <SidebarMenuButton className="cursor-pointer">
                     <BrickWallShield className="w-4 h-4" />
                     <span>Audits</span>
                   </SidebarMenuButton>
                 </AuditDialog>
-              </SidebarMenuItem>
+              </SidebarMenuItem> */}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
